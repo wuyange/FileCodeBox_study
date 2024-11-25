ee40c2086519422940f775e23faafd099bc9231a
shunyu <2939773860@qq.com>
2024-11-25 22:57:53 +0800

    update

--- apps/base/views.py
+++ apps/base/views.py
@@ -131,6 +131,8 @@ async def select_file(data: SelectFileModel, db_session: AsyncSession = Depends(
     has, file_code = await get_code_file_by_code(data.code, db_session=db_session)
     # 检查文件是否存在
     if not has:
+        print(APIResponse(code=404, detail=file_code))
+        print(type(APIResponse(code=404, detail=file_code)))
         return JSONResponse(content=APIResponse(code=404, detail=file_code), status_code=404)
     # 更新文件的使用次数和过期次数
     file_code.used_count += 1
