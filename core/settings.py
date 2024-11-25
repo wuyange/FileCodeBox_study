30410e44dbb1e8276459b118127f6739ee0af3b5
wuyange <2639773860@qq.com>
2024-11-25 21:35:06 +0800

    shunyu update

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ class DefaultSettings(BaseSettings):
     name:str =  '文件快递柜 - FileCodeBox'
     description:str =  '开箱即用的文件快传系统'
     notify_title:str =  '系统通知'
-    notify_content:str =  '欢迎使用 FileCodeBox，本程序开源于 <a href="https://github.com/vastsa/FileCodeBox" target="_blank">Github</a> ，欢迎Star和Fork。'
+    notify_content:str =  '欢迎使用 FileCodeBox'
     page_explain:str =  '请勿上传或分享违法内容。根据《中华人民共和国网络安全法》、《中华人民共和国刑法》、《中华人民共和国治安管理处罚法》等相关规定。 传播或存储违法、违规内容，会受到相关处罚，严重者将承担刑事责任。本站坚决配合相关部门，确保网络内容的安全，和谐，打造绿色网络环境。'
     keywords:str =  'FileCodeBox, 文件快递柜, 口令传送箱, 匿名口令分享文本, 文件'
     robotsText:str = 'User-agent: *\nDisallow: /'
@@ -29,7 +29,7 @@ class DefaultSettings(BaseSettings):
     max_save_seconds:int = 0
     openUpload:int = 1
     textSize:int = 2000
-    uploadSize:int = 1024 * 1024 * 10
+    uploadSize:int = 1024 * 1024 * 10 * 1024
     expireStyle:List[str] = ['day', 'hour', 'minute', 'forever', 'count']
     opacity:float = 0.9
     background:str = ''
